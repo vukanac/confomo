a0f9a3ef2303e84c39afcac47122797574d97a34
Matt Stauffer <matt@tighten.co>
2016-04-13 11:56:57 -0400

    add queue clear command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel {
 	protected $commands = [
 		'Confomo\Console\Commands\Inspire',
 		'Confomo\Console\Commands\GrabTwitterProfilesCommand',
+		'Confomo\Console\Commands\ClearBeanstalkdQueue',
 	];
 
 	/**
